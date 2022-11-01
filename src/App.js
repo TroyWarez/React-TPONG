fc9fde30bdc310c544bb7fe3a2fb7aa7844b8c43
TroyWarez <TroyWarez@troysdomain.com>
2022-11-01 10:12:41 -0700

    Added some stuff.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 let PlayerScore, CompScore;
 const ver = "0.01";
-
 function App() {
-  
+  return <div>TPONG</div>;
+}
+
+function InitGame(Height, Width)
+{
+
 }
 
 export default App;
