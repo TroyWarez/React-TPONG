043061c9ac85aac1a722ba5f31e897779b7fadba
Troy <TroyWarez@troysdomain.com>
2023-10-17 11:25:50 -0700

    Last commit before migration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import  { ReactComponent as DarkModeIcon } from './dark_mode.svg';
+import  { ReactComponent as LightModeIcon } from './light_mode.svg';
 import './App.css';
+import React, { useState } from "react";
+let isDark = false;
 function App() {
-  return (<h1>Hello, world!</h1>);
+    const [style, setStyle] = useState("button"); 
+
+    function clickHandler(){
+        if (!isDark) {
+            document.body.classList.add('dark');
+            isDark = true;
+          } else {
+            document.body.classList.remove('dark');
+            isDark = false;
+          }
+        }
+  return (    <>
+      <div className="App"><button className='buttonDarkModeToggle' onClick= {clickHandler}> <LightModeIcon fill='white'></LightModeIcon></button></div>
+  
+  </>);
 }
 
 export default App;
